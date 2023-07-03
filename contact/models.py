20023e5687e6256ffdcda5d1b1f99eea2264110d
Limber Jhonathan <limberjhonathan@gmail.com>
2023-07-03 20:35:06 -0300

    Criando Banco de Dados

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Contact(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=50)
+    # lembrando que o blank e opcional isso quer dizer que não e obigatorio prencher este campo
+    email = models.EmailField(max_length=254, blank=True)
+    creaeted = models.DateTimeField(default=timezone.now)
+    description = models.TextField(blank=True)

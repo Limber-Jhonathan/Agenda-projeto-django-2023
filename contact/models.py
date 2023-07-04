4fc59c2977dcf57a0a41d86226e6e246a497f8c6
Limber Jhonathan <limberjhonathan@gmail.com>
2023-07-04 17:47:37 -0300

    Incluindo a CATEGORIA

--- contact/models.py
+++ contact/models.py
@@ -2,6 +2,13 @@ from django.db import models
 from django.utils import timezone
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return self.name
+    
+
 
 class Contact(models.Model):
     first_name = models.CharField(max_length=50)
@@ -14,6 +21,11 @@ class Contact(models.Model):
     show = models.BooleanField(default=True)
     #Os arquivos enviados pelo usuario irão para esta pasta
     picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m')
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        blank=True, null=True
+    )
 
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'

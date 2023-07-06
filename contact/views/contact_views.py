417607c07b64994891fe0619638e62579c8ad107
Limber Jhonathan <limberjhonathan@gmail.com>
2023-07-05 22:18:24 -0300

    Implementação dos dados sobre o html

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -3,10 +3,13 @@ from contact.models import Contact
 
 # Create your views here.
 def index(request):
-    contact = Contact.objects.all
+    contact = Contact.objects.filter(show=True).order_by('id')#[:2]
+    # contact = Contact.objects.all
     context = {
         'contacts':contact,
     }
+
+    # print(contact.query)
 
     return render(
         request,

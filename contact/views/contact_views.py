22f17b76643d8056d6303feb7e3800f199321edc
Limber Jhonathan <limberjhonathan@gmail.com>
2023-07-06 14:27:01 -0300

    Inclução da agenda

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from contact.models import Contact
+from django.http import Http404
 
 # Create your views here.
 def index(request):
@@ -14,5 +15,25 @@ def index(request):
     return render(
         request,
         'contact/index.html',
+        context
+    )
+
+def contact(request, contact_id):
+    # single_contact = Contact.objects.get(pk=contact_id)#[:2]
+    # O get retorna um objeto. O filter retorna uma lista.
+    # single_contact = Contact.objects.filter(pk=contact_id).first()
+    single_contact = get_object_or_404(Contact, pk=contact_id, show=True)
+
+    print(single_contact)
+    # contact = Contact.objects.all
+    context = {
+        'contact':single_contact,
+    }
+
+    # print(contact.query)
+
+    return render(
+        request,
+        'contact/contact.html',
         context
     )

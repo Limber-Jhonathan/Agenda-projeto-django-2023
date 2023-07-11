27f974a9970d3a81550e4618c3909607aa047180
Limber Jhonathan <limberjhonathan@gmail.com>
2023-07-11 14:10:19 -0300

    Implementando o message

--- contact/views/user_forms.py
+++ contact/views/user_forms.py
@@ -1,15 +1,18 @@
-from  django.shortcuts import render
-
+from  django.shortcuts import render, redirect
+from django.contrib import messages
 from  contact.forms import RegisterForm
 
 def register(request):
-
+    #manda mensagens
+    messages.info(request, "Um texto qualquer")
     form = RegisterForm()
 
     if request.method == 'POST':
         form  = RegisterForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, "Usuário registrado com sucesso")
+            return redirect('contact:index')
 
     return render(
         request,
